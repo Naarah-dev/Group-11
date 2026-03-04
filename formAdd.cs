521b4f7e70356beb4a4360d6610bfe2c27983cad
Naarah-dev <thinhuy1@dtu.edu.vn>
2026-03-04 10:59:23 +0700

    Refactor formAdd.cs and remove unnecessary lines

--- formAdd.cs
+++ formAdd.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System; //pls review this code
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -44,3 +44,4 @@ namespace QLSV
         }
     }
 }
+
